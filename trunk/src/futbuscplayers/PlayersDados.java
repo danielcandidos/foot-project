f07877ee13d91cf256214de2764c31cb8568e729
jrjcsj@gmail.com <jrjcsj@gmail.com@a6a1c239-990e-1f58-cd6a-9e72c88e0774>
2013-01-16 03:28:52 +0000

    Foot Project

---
+++ trunk/src/futbuscplayers/PlayersDados.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package futbuscplayers;
+
+/**
+ *
+ * @author Daniel
+ */
+public class PlayersDados {
+    String dataDeNascimento, altura, peso, clubeAtual, ultimoClube;
+    
+}
