4745a216b42e94c4b352f28a49b932883611a79c
jrjcsj <jrjcsj@gmail.com>
2013-03-21 18:58:58 +0000

    Tabela Temporada 2009

---
+++ src/SQL/temporada_2009.sql
@@ -0,0 +1,10 @@
+
+create table if not exists temporada_2009 (
+nome_jogador varchar (60),
+clube_2009 varchar(40),
+jogos_2009 int (5),
+gols_2009 int(4),
+passes_2009 int(4),
+ca_2009 int(6),
+cv_2009 int(6),
+ano int(7))
