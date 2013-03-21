2f482213f802d160e23561b3ccb7bf0511ac2d06
jrjcsj@gmail.com <jrjcsj@gmail.com@a6a1c239-990e-1f58-cd6a-9e72c88e0774>
2013-03-21 19:00:33 +0000

    Tabela Temporada 2011

---
+++ trunk/src/SQL/temporada_2011.sql
@@ -0,0 +1,10 @@
+
+create table if not exists temporada_2011 (
+nome_jogador varchar (60),
+clube_2011 varchar(40),
+jogos_2011 int (5),
+gols_2011 int(4),
+passes_2011 int(4),
+ca_2011 int(6),
+cv_2011 int(6),
+ano int(7))
