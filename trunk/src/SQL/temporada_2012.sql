eff9c8af8dd5b1c266283f61cd05a10a58b2f2dc
jrjcsj@gmail.com <jrjcsj@gmail.com@a6a1c239-990e-1f58-cd6a-9e72c88e0774>
2013-03-21 19:01:38 +0000

    Tabela Temporada 2012

---
+++ trunk/src/SQL/temporada_2012.sql
@@ -0,0 +1,10 @@
+
+create table if not exists temporada_2012 (
+nome_jogador varchar (60),
+clube_2012 varchar(40),
+jogos_2012 int (5),
+gols_2012 int(4),
+passes_2012 int(4),
+ca_2012 int(6),
+cv_2012 int(6),
+ano int(7))
